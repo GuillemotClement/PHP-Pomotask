6a523a8d21583b56f230f23ec5754117f1713175
GuillemotClement <guillemot.clement@protonmail.com>
2025-03-09 21:37:53 +0100

    feat: add menu connected

--- src/Views/layout/partials/header.php
+++ src/Views/layout/partials/header.php
@@ -9,10 +9,13 @@
     <li><a href="/">Taches</a></li>
   </ul>
   <ul class="header-menu-action">
-    <li><a href="/login" class="btn">Connexion</a></li>
-    <li><a href="/" class="btn">Inscription</a></li>
-    <li><a href="/" class="btn">Profil</a></li>
-    <li><a href="/logout" class="btn">Deconnexion</a></li>
+    <?php if($_SESSION['user_id'] > 0): ?>
+      <li><a href="/" class="btn">Profil</a></li>
+      <li><a href="/logout" class="btn">Deconnexion</a></li>
+    <?php else: ?>
+      <li><a href="/login" class="btn">Connexion</a></li>
+      <li><a href="/" class="btn">Inscription</a></li>
+    <?php endif; ?>
   </ul>
   <div class="header-action" id="menu_mobile">
     <i class="fa-solid fa-bars"></i>
